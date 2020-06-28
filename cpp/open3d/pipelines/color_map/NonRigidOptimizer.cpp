5557c57db89381c7ba240c7da3ab3989d6a2b100
Yixing Lao <yixing.lao@gmail.com>
2020-06-27 21:01:17 -0700

    move

--- cpp/open3d/pipelines/color_map/NonRigidOptimizer.cpp
+++ cpp/open3d/pipelines/color_map/NonRigidOptimizer.cpp
@@ -49,6 +49,71 @@ static std::vector<ImageWarpingField> CreateWarpingFields(
     return fields;
 }
 
+/// Function to compute JTJ and Jtr
+/// Input: function pointer f and total number of rows of Jacobian matrix
+/// Output: JTJ, JTr, sum of r^2
+/// Note: this function is almost identical to the functions in
+/// Utility/Eigen.h/cpp, but this function takes additional multiplication
+/// pattern that can produce JTJ having hundreds of rows and columns.
+template <typename VecInTypeDouble,
+          typename VecInTypeInt,
+          typename MatOutType,
+          typename VecOutType>
+static std::tuple<MatOutType, VecOutType, double> ComputeJTJandJTrNonRigid(
+        std::function<void(int, VecInTypeDouble&, double&, VecInTypeInt&)> f,
+        int iteration_num,
+        int nonrigidval,
+        bool verbose /*=true*/) {
+    MatOutType JTJ(6 + nonrigidval, 6 + nonrigidval);
+    VecOutType JTr(6 + nonrigidval);
+    double r2_sum = 0.0;
+    JTJ.setZero();
+    JTr.setZero();
+#ifdef _OPENMP
+#pragma omp parallel
+    {
+#endif
+        MatOutType JTJ_private(6 + nonrigidval, 6 + nonrigidval);
+        VecOutType JTr_private(6 + nonrigidval);
+        double r2_sum_private = 0.0;
+        JTJ_private.setZero();
+        JTr_private.setZero();
+        VecInTypeDouble J_r;
+        VecInTypeInt pattern;
+        double r;
+#ifdef _OPENMP
+#pragma omp for nowait
+#endif
+        for (int i = 0; i < iteration_num; i++) {
+            f(i, J_r, r, pattern);
+            for (auto x = 0; x < J_r.size(); x++) {
+                for (auto y = 0; y < J_r.size(); y++) {
+                    JTJ_private(pattern(x), pattern(y)) += J_r(x) * J_r(y);
+                }
+            }
+            for (auto x = 0; x < J_r.size(); x++) {
+                JTr_private(pattern(x)) += r * J_r(x);
+            }
+            r2_sum_private += r * r;
+        }
+#ifdef _OPENMP
+#pragma omp critical
+        {
+#endif
+            JTJ += JTJ_private;
+            JTr += JTr_private;
+            r2_sum += r2_sum_private;
+#ifdef _OPENMP
+        }
+    }
+#endif
+    if (verbose) {
+        utility::LogDebug("Residual : {:.2e} (# of elements : {:d})",
+                          r2_sum / (double)iteration_num, iteration_num);
+    }
+    return std::make_tuple(std::move(JTJ), std::move(JTr), r2_sum);
+}
+
 void NonRigidOptimizer::Run(const NonRigidOptimizerOption& option) {
     utility::LogDebug("[ColorMapOptimization] :: MakingMasks");
     auto images_mask = CreateDepthBoundaryMasks(
